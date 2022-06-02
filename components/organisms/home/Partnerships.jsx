06da70d88c88cd8b176efa4495a054dba36c4f83
mwogates <michaelwonggates@gmail.com>
2022-06-02 15:21:05 -0700

    Update partnerships section

--- components/organisms/home/Partnerships.jsx
+++ components/organisms/home/Partnerships.jsx
@@ -7,68 +7,56 @@ const Partnerships = () => {
         <div className="mb-4">
           <h1 className="text-4xl text-center mb-8">Partnerships</h1>
           <p className="text-lg text-center mb-16">
-            The Revenants will be partnering with collections for in-game
-            abilities, integragtions and loot.
+            The Revenants has partnerships with projects across Solana to
+            provide holders of those projects with in-game power-ups, custom
+            levels, and exclusive in-game characters.
           </p>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2">
           <div className="flex flex-col items-center mb-12">
-            <img
-              src="/media/sovana.png"
-              alt="sovana"
-              loading="lazy"
-              className="w-3/5 mb-12"
-            />
-            <div className="px-4 flex flex-col justify-center items-center">
-              <a
-                href="https://www.sovana.world/"
-                rel="noreferrer"
-                target="_blank"
-                className="mb-12"
-              >
-                <div className="w-96 h-40 relative flex justify-center items-center cursor-pointer flex-shrink-0 hover:text-brand-purple">
-                  <img
-                    className="absolute top-0 left-0 w-full h-full"
-                    src="/media/button.png"
-                    alt="button"
-                  ></img>
-                  <span className="text-lg mb-3 z-10">SOVANA</span>
-                </div>
-              </a>
-              <h1 className="text-4xl text-center mb-4">Sovana</h1>
+            <div className="w-4/5 h-60 mb-8 pb-8 border-b-4 border-brand-purple">
+              <img
+                src="/media/partners/Cyber_Samurai.png"
+                alt="Cyber Samurai"
+                loading="lazy"
+                className="w-full h-full object-contain"
+              />
+            </div>
+            <div className="px-2 flex flex-col justify-center items-center">
               <p className="text-lg text-center px-4">
-                Our game will be the first playable game in Sovana! Sovana
-                holders will also receive in-game benefits.
+                Protecting Degens and bringing Harmony to the Metaverse.
+                <br />
+                <br /> The Cyber Samurai are military nobility of DeFi, on a
+                mission to protect degens from rug pulls and bring harmony to
+                the metaverse. In our first comic we tell the story of Solana’s
+                first year on mainnet.Their mission is to onboard new users to
+                DeFi via NFTs. Using our comic book, software tools, and strong
+                community, we help educate users and give them resources to
+                navigate the metaverse.
               </p>
             </div>
           </div>
           <div className="flex flex-col items-center mb-12">
-            <img
-              src="/media/coolCats.png"
-              alt="sovana"
-              loading="lazy"
-              className="w-3/5 mb-12"
-            />
-            <div className="px-4 flex flex-col justify-center items-center">
-              <a
-                href="https://www.dopecats.com/"
-                rel="noreferrer"
-                target="_blank"
-                className="mb-12"
-              >
-                <div className="w-96 h-40 relative flex justify-center items-center cursor-pointer flex-shrink-0 hover:text-brand-purple">
-                  <img
-                    className="absolute top-0 left-0 w-full h-full"
-                    src="/media/button.png"
-                    alt="button"
-                  ></img>
-                  <span className="text-lg mb-3 z-10">DOPE CATS</span>
-                </div>
-              </a>
-              <h1 className="text-4xl text-center mb-4">Dope Cats</h1>
+            <div className="w-4/5 h-60 mb-8 pb-8 border-b-4 border-brand-purple">
+              <img
+                src="/media/partners/hippos.png"
+                alt="hippos"
+                loading="lazy"
+                className="w-full h-full object-contain"
+              />
+            </div>
+            <div className="px-2 flex flex-col justify-center items-center">
               <p className="text-lg text-center px-4">
-                Dope cat holders will receive in-game benefits and 1 rugged
-                revenant NFT.
+                The High Roller Hippo Clique(HRHC) is composed of 7,777 hippos
+                living on the Solana blockchain. HRHC is a community-run
+                organization in which everyone provides value back to the
+                community in return for their stake in our decentralized
+                autonomous organization(DAO). Throughout HRHC's project
+                lifecycle, it will not be operated, decided upon, or controlled
+                by any one individual. HRHC has assembled an incredibly strong,
+                grass-roots team of like minded individuals, who all share a
+                common vision of decentralizing voting power and building new
+                tech.
               </p>
             </div>
           </div>
