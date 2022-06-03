3fecc52f7c78e8be7d330bd7f21330c0e1974d68
mwogates <michaelwonggates@gmail.com>
2022-06-02 17:08:55 -0700

    update partnerships section with new assets

--- components/organisms/home/Partnerships.jsx
+++ components/organisms/home/Partnerships.jsx
@@ -12,41 +12,58 @@ const Partnerships = () => {
             levels, and exclusive in-game characters.
           </p>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2">
-          <div className="flex flex-col items-center mb-12">
-            <div className="w-4/5 h-60 mb-8 pb-8 border-b-4 border-brand-purple">
+        <div className="flex flex-wrap justify-evenly">
+          <div className="w-full md:w-[32%] flex flex-col items-center mb-12">
+            <div className="w-3/5 h-40 mb-8 pb-4 border-b-4 border-brand-purple">
               <img
-                src="/media/sovana.png"
+                src="/media/partners/Sovana-Logo.png"
                 alt="Cyber Samurai"
                 loading="lazy"
                 className="w-full h-full object-contain"
               />
             </div>
             <div className="px-2 flex flex-col justify-center items-center">
-              <p className="text-lg text-center px-4">
+              <p className="text-sm text-center px-4">
                 Our game will be the first playable game in Sovana! Sovana
                 holders will also receive in-game benefits.
               </p>
             </div>
           </div>
-          <div className="flex flex-col items-center mb-12">
-            <div className="w-4/5 h-60 mb-8 pb-8 border-b-4 border-brand-purple">
+          <div className="w-full md:w-[32%] flex flex-col items-center mb-12">
+            <div className="w-3/5 h-40 mb-8 pb-4 border-b-4 border-brand-purple">
               <img
-                src="/media/coolCats.png"
+                src="/media/partners/PixelBands-Logo.png"
                 alt="hippos"
                 loading="lazy"
                 className="w-full h-full object-contain"
               />
             </div>
             <div className="px-2 flex flex-col justify-center items-center">
-              <p className="text-lg text-center px-4">
+              <p className="text-sm text-center px-4">
+                PixelBands is the first generative art and generative music
+                collection on Solana. 4,444 Music X Art Band Members playing
+                either guitar, bass, drums, or keys.
+              </p>
+            </div>
+          </div>
+          <div className="w-full md:w-[32%] flex flex-col items-center mb-12">
+            <div className="w-3/5 h-40 mb-8 pb-4 border-b-4 border-brand-purple">
+              <img
+                src="/media/partners/DopeCats-Logo.png"
+                alt="hippos"
+                loading="lazy"
+                className="w-full h-full object-contain"
+              />
+            </div>
+            <div className="px-2 flex flex-col justify-center items-center">
+              <p className="text-sm text-center px-4">
                 Dope cat holders will receive in-game benefits and 1 rugged
                 revenant NFT.
               </p>
             </div>
           </div>
-          <div className="flex flex-col items-center mb-12">
-            <div className="w-4/5 h-60 mb-8 pb-8 border-b-4 border-brand-purple">
+          <div className="w-full md:w-[40%] flex flex-col items-center mb-12">
+            <div className="w-3/5 h-48 mb-8 pb-4 border-b-4 border-brand-purple">
               <img
                 src="/media/partners/Cyber_Samurai.png"
                 alt="Cyber Samurai"
@@ -55,21 +72,17 @@ const Partnerships = () => {
               />
             </div>
             <div className="px-2 flex flex-col justify-center items-center">
-              <p className="text-lg text-center px-4">
-                Protecting Degens and bringing Harmony to the Metaverse.
-                <br />
-                <br /> The Cyber Samurai are military nobility of DeFi, on a
-                mission to protect degens from rug pulls and bring harmony to
-                the metaverse. In our first comic we tell the story of Solana’s
+              <p className="text-sm text-center px-4">
+                The Cyber Samurai are military nobility of DeFi, on a mission to
+                protect degens from rug pulls and bring harmony to the
+                metaverse. In our first comic we tell the story of Solana’s
                 first year on mainnet.Their mission is to onboard new users to
-                DeFi via NFTs. Using our comic book, software tools, and strong
-                community, we help educate users and give them resources to
-                navigate the metaverse.
+                DeFi via NFTs.
               </p>
             </div>
           </div>
-          <div className="flex flex-col items-center mb-12">
-            <div className="w-4/5 h-60 mb-8 pb-8 border-b-4 border-brand-purple">
+          <div className="w-full md:w-[40%] flex flex-col items-center mb-12">
+            <div className="w-3/5 h-48 mb-8 pb-4 border-b-4 border-brand-purple">
               <img
                 src="/media/partners/hippos.png"
                 alt="hippos"
@@ -78,17 +91,12 @@ const Partnerships = () => {
               />
             </div>
             <div className="px-2 flex flex-col justify-center items-center">
-              <p className="text-lg text-center px-4">
+              <p className="text-sm text-center px-4">
                 The High Roller Hippo Clique(HRHC) is composed of 7,777 hippos
                 living on the Solana blockchain. HRHC is a community-run
                 organization in which everyone provides value back to the
                 community in return for their stake in our decentralized
-                autonomous organization(DAO). Throughout HRHC's project
-                lifecycle, it will not be operated, decided upon, or controlled
-                by any one individual. HRHC has assembled an incredibly strong,
-                grass-roots team of like minded individuals, who all share a
-                common vision of decentralizing voting power and building new
-                tech.
+                autonomous organization(DAO).
               </p>
             </div>
           </div>
