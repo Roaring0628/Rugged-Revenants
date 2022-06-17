e5d62652ce5474eedc09449cee8f74e519ee35cf
mwogates <michaelwonggates@gmail.com>
2022-06-17 07:45:25 -0700

    Update game integration again

--- components/organisms/home/Demo.jsx
+++ components/organisms/home/Demo.jsx
@@ -1,18 +1,77 @@
 /* eslint-disable @next/next/no-img-element */
-import { useEffect, useRef } from "react";
+import { useEffect, useState } from "react";
 
 const Demo = ({ handlePlay }) => {
-  const demoIframeRef = useRef();
+  const [myGameInstance, setMyGameInstance] = useState(null);
 
-  const handleImageClick = () => {
-    demoIframeRef.current.focus();
+  useEffect(() => {
+    window.addEventListener(
+      "message",
+      (event) => {
+        if (event.origin !== window.location.origin) return;
+
+        if (
+          event.data &&
+          typeof event.data === "object" &&
+          event.data.key === "unity-message"
+        ) {
+          console.log("========= UNITY MESSAGE =========");
+          console.log(event.data);
+        }
+      },
+      false
+    );
+
+    // For new build of game: Need to update loaderUrl, and download build files and replace them
+    let loaderUrl =
+      "https://v6p9d9t4.ssl.hwcdn.net/html/5937995/RuggedWebGL_TESTING/Build/RuggedWebGL_TESTING.loader.js";
+    let script = document.createElement("script");
+    script.src = loaderUrl;
+    script.id = "demo-game-script";
+    script.onload = () => {
+      window
+        .createUnityInstance(document.querySelector("#unity-canvas"), {
+          dataUrl: "/Build/RuggedWebGL_TESTING.data.unityweb",
+          frameworkUrl: "/Build/RuggedWebGL_TESTING.framework.js.unityweb",
+          codeUrl: "/Build/RuggedWebGL_TESTING.wasm.unityweb",
+          streamingAssetsUrl: "StreamingAssets",
+          companyName: "DefaultCompany",
+          productName: "Dope Cats",
+          productVersion: "1.0",
+          matchWebGLToCanvasSize: false, // Uncomment this to separately control WebGL canvas render size and DOM element size.
+          // devicePixelRatio: 1, // Uncomment this to override low DPI rendering on high DPI displays.
+        })
+        .then((unityInstance) => {
+          window.myGameInstance = unityInstance;
+          setMyGameInstance(unityInstance);
+        });
+    };
+    document.body.appendChild(script);
+  }, []);
+
+  const sendMessageToGameInstance = () => {
+    let testObject = { hasDopeCat: true, hasPixelBand: false };
+    let jsonString = JSON.stringify(testObject);
+    myGameInstance.SendMessage(
+      "JavascriptHook",
+      "RecieveWalletJson",
+      jsonString
+    );
+  };
+
+  const quitGame = () => {
+    myGameInstance.Quit();
+    const scriptEl = document.querySelector("#demo-game-script");
+    if (scriptEl) scriptEl.remove();
+    setMyGameInstance(null);
+    window.myGameInstance = null;
+    handlePlay();
   };
 
   useEffect(() => {
-    setTimeout(() => {
-      handleImageClick();
-    }, 200);
-  }, []);
+    if (myGameInstance) sendMessageToGameInstance();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [myGameInstance]);
 
   return (
     <div className="game-wrapper">
@@ -23,25 +82,16 @@ const Demo = ({ handlePlay }) => {
           alt="tv screen"
           style={{ pointerEvents: "none" }}
         ></img>
-        <iframe
-          ref={demoIframeRef}
-          title="demo"
-          mozallowfullscreen="true"
-          allow="autoplay; fullscreen *; geolocation; microphone; camera; midi; monetization; xr-spatial-tracking; gamepad; gyroscope; accelerometer; xr; cross-origin-isolated"
-          frameBorder="0"
-          width="960px"
-          height="600px"
-          src="https://v6p9d9t4.ssl.hwcdn.net/html/5902926/RuggedWebGL/index.html"
-          msallowfullscreen="true"
-          scrolling="no"
-          allowFullScreen={true}
-          webkitallowfullscreen="true"
-          allowtransparency="true"
-        ></iframe>
+        <canvas
+          id="unity-canvas"
+          width="960"
+          height="600"
+          style={{ width: 960, height: 600, background: "#231f20" }}
+        ></canvas>
       </div>
       <div className="quit-wrapper">
         <div
-          onClick={handlePlay}
+          onClick={quitGame}
           className="w-64 h-24 relative flex justify-center items-center cursor-pointer hover:text-brand-purple"
         >
           <img
