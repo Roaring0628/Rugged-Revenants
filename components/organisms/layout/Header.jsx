e77fcf91a6e80176488c2ecd388906075aa4fbb1
mwogates <michaelwonggates@gmail.com>
2022-06-19 12:24:57 -0700

    fix eslint

--- components/organisms/layout/Header.jsx
+++ components/organisms/layout/Header.jsx
@@ -29,6 +29,7 @@ export default function Header() {
     if(publicKey) {
       fetchData()
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [publicKey]);
 
   const fetchData = async () => {
