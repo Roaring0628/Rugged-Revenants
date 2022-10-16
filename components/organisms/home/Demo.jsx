d1d04cb25896b247d7ce11bfa9a7ad807f195557
mwogates <michaelwonggates@gmail.com>
2022-10-16 07:27:16 -0700

    Update game 6662112

--- components/organisms/home/Demo.jsx
+++ components/organisms/home/Demo.jsx
@@ -51,7 +51,7 @@ const Demo = ({
   useEffect(() => {
     window.addEventListener("message", handleMessage);
 
-    console.log('Game Version: 6652288');
+    console.log('Game Version: 6662112');
 
     // For new build of game: Need to update loaderUrl, and download build files and replace them
     // Live loaderUrl
